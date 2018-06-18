b67d782585e8702863ae6a1cdbaf53f6492f8489
Clark Dwain Luna <clark_luna@SurveySampling.com>
2018-06-18 17:43:27 -0600

    update

--- src/js/components/container/App.js
+++ src/js/components/container/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
+import Button from '@material-ui/core/Button';
+import Drawer from './Drawer';
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <p>THIS IS APP JS</p>
-            </div>
+            <Drawer />
         )
     }
 }

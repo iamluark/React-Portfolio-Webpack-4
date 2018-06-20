02296c1d8b64ae9ca51172d5a4f67c9384d4248c
Clark Dwain Luna <clark_luna@SurveySampling.com>
2018-06-19 19:08:48 -0600

    fixes

--- src/app/index.js
+++ src/app/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Router } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
+//import { createBrowserHistory } from 'history';
 
 import Routes from "./routes/routes";
 
-var hist = createBrowserHistory();
+//var hist = createBrowserHistory();
 
 ReactDOM.render(
-    <Router history={hist}>
+    <Router>
         <Routes />
     </Router>,
     document.getElementById("root")

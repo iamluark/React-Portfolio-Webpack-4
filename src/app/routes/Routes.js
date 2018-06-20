da7dfc88557e1e2b2b14998e3ec1f36e7ebad94b
Clark Dwain Luna <clark_luna@SurveySampling.com>
2018-06-20 11:04:31 -0600

    fixes

--- src/app/routes/Routes.js
+++ src/app/routes/Routes.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { Router, Route, Switch, hashHistory } from 'react-router';
+import { Router, Route, Switch } from 'react-router';
 import { HashRouter } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
+
+const history = createBrowserHistory();
 
 import App from 'app/App'
 import Home from 'containers/Home';
@@ -10,7 +13,7 @@ import PageNotFound from './PageNotFound';
 
 export default function Routes() {
     return (
-        <HashRouter history={hashHistory}>
+        <HashRouter history={history}>
             <Switch>
                 <Route path="/" component={Home} exact />
                 <Route path="/about" component={About} />

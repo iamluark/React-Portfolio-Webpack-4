061c5a48f2cce073090b3788175441a5f7567634
Clark Dwain Luna <clark_luna@SurveySampling.com>
2018-06-20 12:48:46 -0600

    test hashRoute

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     },
     entry: './src/app/index.js',
     output: {
-        path: path.join(__dirname, 'dist'),
+        path: __dirname,
         publicPath: '/',
         filename: 'dist/js/bundle.js'
     },

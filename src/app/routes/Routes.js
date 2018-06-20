8151c041542db3ce352cc01e4d8ae41ea0b10934
Clark Dwain Luna <clark_luna@SurveySampling.com>
2018-06-20 11:08:38 -0600

    live update

--- src/app/routes/Routes.js
+++ src/app/routes/Routes.js
@@ -14,12 +14,12 @@ import PageNotFound from './PageNotFound';
 export default function Routes() {
     return (
         <HashRouter history={history}>
-            <Switch>
+            <div>
                 <Route path="/" component={Home} exact />
                 <Route path="/about" component={About} />
                 <Route path="/contact" component={Contact} />
                 <Route component={PageNotFound} />
-            </Switch>
+            </div>
         </HashRouter>
     );
 }
